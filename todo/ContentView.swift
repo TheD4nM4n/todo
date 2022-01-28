4f1fa3c60eb5974dd10eea98593eaa6b478b6c97
Daniel Poe <thed4nm4n@gmail.com>
2022-01-28 12:44:42 -0500

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Daniel Jaye Poe (Student) on 1/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Daniel Jaye Poe (Student) on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
